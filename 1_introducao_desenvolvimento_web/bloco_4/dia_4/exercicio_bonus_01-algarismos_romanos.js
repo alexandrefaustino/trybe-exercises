be93bb29124168e3b426e74052054a0d8f3685da
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-07 14:17:42 -0300

    atualização de código da função bonus

--- 1_introducao_desenvolvimento_web/bloco_4/dia_4/exercicio_bonus_01-algarismos_romanos.js
+++ 1_introducao_desenvolvimento_web/bloco_4/dia_4/exercicio_bonus_01-algarismos_romanos.js
@@ -1,36 +1,35 @@
-romansNumber = {
+const romansNumber = {
   I: 1,
   V: 5,
   X: 10,
   L: 50,
   C: 100,
   D: 500,
-  M: 1000
+  M: 1000,
 }
 
-function transformsNumberRomanos(number){
-  for(let i in romansNumber){
-    if(number === romansNumber[i]){
-      console.log(i);
-      break;
-    }           
+function transformsNumberRomanos(numbersRomans) {
+  numbersRomans = numbersRomans.toUpperCase();
+  numbersRomans = numbersRomans.replace('IV','IIII')
+  numbersRomans = numbersRomans.replace('IX','VIIII')
+  numbersRomans = numbersRomans.replace('XL','XXXX')
+  numbersRomans = numbersRomans.replace('XC','LXXXX')
+  numbersRomans = numbersRomans.replace('CD','CCCC')
+  numbersRomans = numbersRomans.replace('CM','DCCCC')
+  return numbersRomans;
+}  
+
+function valueNumbersRomans(numbersRomans) {
+  numbersRomans = transformsNumberRomanos(numbersRomans);
+  let outputNumber = 0
+  for (let number of numbersRomans) {
+    for (let key in romansNumber) {
+      if ( number === key) {
+        outputNumber += romansNumber[key];
+      }
+    }
   }
+  return outputNumber;
 }
-transformsNumberRomanos(23);
-let dividendo = 23;
-let divisor = 10;
-let resto;
-let quociente;
 
-resto = dividendo %  divisor;
-quociente = dividendo - resto;
-quociente = (dividendo - resto) / divisor;
-console.log(resto);
-console.log(quociente);
-
-let outputNumber =0;
-console.log(romansNumber.X);
-
-outputNumber += romansNumber.X
-outputNumber += romansNumber.X
-console.log(outputNumber);
+console.log(valueNumbersRomans('CM'));
