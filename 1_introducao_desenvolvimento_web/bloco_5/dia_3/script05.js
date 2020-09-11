4964f46abef8d01949903e14b620af64f9c576ba
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-11 20:10:28 -0300

    resolução do exercício 05

---
+++ 1_introducao_desenvolvimento_web/bloco_5/dia_3/script05.js
@@ -0,0 +1,37 @@
+var personName = "";
+
+function complainMessage() {
+  alert("I told you not to do that " + personName);
+}
+
+function typeAndScream() {
+  document.getElementById("audio").play();
+  personName = document.getElementById("name").value;
+}
+
+function goDark() {
+  var body = document.getElementsByTagName("body")[0];
+  if (body.className == "") {
+    body.className = "dark";
+  }
+  else {
+    body.className = "";
+  }
+  
+}
+
+function finishTheStory() {
+  document.getElementById("story").innerHTML = "So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!";
+}
+
+let eclipse = document.querySelector('.eclipse');
+eclipse.addEventListener('click', goDark);
+
+let btnDontClick = document.querySelector('.dont-click');
+btnDontClick.addEventListener('click', complainMessage);
+
+let inputName = document.getElementById('name');
+inputName.oninput = typeAndScream;
+
+let btnTellMe = document.querySelector('.teel-me');
+btnTellMe.addEventListener('click', finishTheStory);
