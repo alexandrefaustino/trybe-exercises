4c365f629129b217575ee49bc52e37d3c1741a39
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-21 12:10:14 -0300

    criação da parte 01 do form

---
+++ 1_introducao_desenvolvimento_web/bloco_6/dia_1/cript.js
@@ -0,0 +1,44 @@
+const estados = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+}
+
+const getTagSelected = document.getElementById('estados');
+
+for (let key in estados) {
+  const tagOption = document.createElement('option');
+  tagOption.value = key;
+  tagOption.innerText = estados[key];
+  getTagSelected.appendChild(tagOption);
+}
+
+function stopDefAction(event) {
+  event.preventDefault();
+}
+
+document.getElementById('btn').addEventListener('click', stopDefAction, false);
