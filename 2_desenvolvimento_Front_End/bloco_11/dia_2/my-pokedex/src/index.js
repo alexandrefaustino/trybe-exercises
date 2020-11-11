ec6b340eeb929de865f59cd42246e9c042dab03a
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-11 16:04:24 -0300

    Criação dos elementos React Pokemon e Image

--- 2_desenvolvimento_Front_End/bloco_11/dia_2/my-pokedex/src/index.js
+++ 2_desenvolvimento_Front_End/bloco_11/dia_2/my-pokedex/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './Pokemon.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
