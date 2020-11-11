a896e65c362fcb431180690ffda9abce2f189375
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-10 22:12:06 -0300

    resolução do exercício 01

--- 2_desenvolvimento_front_end/dia_1/exercises_11-1/src/App.js
+++ 2_desenvolvimento_front_end/dia_1/exercises_11-1/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+const arrayTask = ['finanças', 'ler à bíblia', 'organizar guarda roupa', 'pegar carteira de doador'];
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {arrayTask.map((item, index) => {
+        return <p key={index}>{task(item)}</p> 
+        }) 
+      }
     </div>
   );
 }
