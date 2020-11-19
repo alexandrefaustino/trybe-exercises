a8f046a853eaf5a23f958911323704e3878a90ab
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-18 21:24:59 -0300

    inclusão dos campos para endereço

--- 2_desenvolvimento_front_end/bloco_12/dia_2/my-form-2.0/src/Forms.jsx
+++ 2_desenvolvimento_front_end/bloco_12/dia_2/my-form-2.0/src/Forms.jsx
@@ -16,28 +16,72 @@ class Forms extends Component {
         initialValues={{
           name: "",
           email: "",
-          cpf: ""
+          cpf: "",
+          address: "",
+          city: "",
+          state: "",
+          type: "",
+          picked: false
         }}
         render={({ values, errors }) => (
           <Form>
-            <h1 className="title">Formulario</h1>
+            <h1 className="title">Formulário</h1>
             <fieldset>
               <div className="div">
                 <label>Nome</label>
-                <Field type="text" name="name" className="inputName"/>
+                <Field type="text" name="name" placeholder="nome" className="inputName"/>
                 {errors.name && (<span className="span">{errors.name}</span>)}
               </div>
               
               <div className="div">
                 <label>Email</label>
-                <Field type="email" name="email" className="inputEmail"/>
+                <Field type="email" name="email" placeholder="fulano@gmail.com" className="inputEmail"/>
                 {errors.email && (<span className="span">{errors.email}</span>)} 
               </div>
 
               <div className="div">
                 <label>CPF</label>
-                <Field type="text" name="cpf" className="inputCpf"/>
+                <Field type="text" name="cpf" placeholder="apenas números" className="inputCpf"/>
                 {errors.cpf && (<span className="span">{errors.cpf}</span>)} 
+              </div>
+
+              <div className="div">
+                <label>Endereço</label>
+                <Field type="text" name="address" placeholder="apenas letras" className="inputAddress"/>
+                {errors.address && (<span className="span">{errors.address}</span>)} 
+              </div>
+
+              <div className="div">
+                <label>Cidade</label>
+                <Field type="text" name="city" placeholder="apenas letras" className="inputCity"/>
+                {errors.city && (<span className="span">{errors.city}</span>)} 
+              </div>
+
+              <div className="div">
+                <label>Estado</label>
+                <Field as="select" name="state" className="inputState">
+                  <option value=""></option>
+                  <option value="pb">Paraíba</option>
+                  <option value="sp">São Paulo</option>
+                  <option value="rn">Ceará</option>
+                  <option value="am">Amazonas</option>
+                  <option value="go">Goias</option>
+                  <option value="rs">Rio Grande do Sul</option>
+                </Field>
+                {errors.state && (<span className="span">{errors.state}</span>)} 
+              </div>
+
+              <div id="my-radio-group">Tipo</div>
+                <div role="group" aria-labelledby="my-radio-group">
+                  <label>
+                    <Field type="radio" name="picked" value="One" />
+                      Casa
+                  </label>
+                
+                  <label>
+                    <Field type="radio" name="picked" value="Two" />
+                      Apartamento
+                  </label>                
               </div>
               
             </fieldset>
--- 2_desenvolvimento_front_end/bloco_12/dia_2/my-form-2.0/src/schema.js
+++ 2_desenvolvimento_front_end/bloco_12/dia_2/my-form-2.0/src/schema.js
@@ -6,7 +6,24 @@ export default Yup.object().shape({
     .max(40, 'máximo 05 caracteres')
     .required('*'),
   email: Yup.string()
-    .email().max(50).required('*'),
+    .email()
+    .max(50)
+    .required('*'),
   cpf: Yup.string()
-    .min(11).max(11).required('*')
+    .matches(/^\d+$/, 'não é um número')
+    .max(11)
+    .min(11)
+    .required('*'),
+  address: Yup.string()
+    .max(200)
+    .matches(/[a-z]/gi, 'apenas letras')
+    .required('*'),
+  city: Yup.string()
+    .max(28)
+    .matches(/[a-z]/gi, 'apenas letras')
+    .required('*'),
+  state: Yup.string()
+    .required('*'),
+  picked: Yup.boolean
+    .required('*'),
 })
