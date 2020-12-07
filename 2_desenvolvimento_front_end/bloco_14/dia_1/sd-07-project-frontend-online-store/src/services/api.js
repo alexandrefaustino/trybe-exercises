e81ec3d0814d637d8229f872c5fa31194736732c
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-12-07 19:35:17 -0300

    Projeto bloco 14 frontEndOlineStore

---
+++ 2_desenvolvimento_front_end/bloco_14/dia_1/sd-07-project-frontend-online-store/src/services/api.js
@@ -0,0 +1,17 @@
+const getFetch = (url) => fetch(url).then((element) => element.json());
+
+export async function getCategories() {
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  try {
+    const promisse = await getFetch(url);
+    return promisse;
+  } catch (err) { return 'Erro'; }
+}
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  try {
+    const promisse = await getFetch(url);
+    return promisse;
+  } catch (err) { return 'Erro'; }
+}
