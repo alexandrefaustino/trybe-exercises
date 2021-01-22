fb24048c4182aabaf9a8e49e26fe62a92209123f
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-01-22 19:21:21 -0300

    exercicio 01 e 02 bloco 18.1

---
+++ 2_desenvolvimento_front_end/bloco_18/dia_1/exercise-contextAPI-refactoring/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Cars from './Cars';
+
+function App() {
+  return (
+    <div className="container">
+      <Cars />     
+    </div>
+  );
+}
+
+export default App;
