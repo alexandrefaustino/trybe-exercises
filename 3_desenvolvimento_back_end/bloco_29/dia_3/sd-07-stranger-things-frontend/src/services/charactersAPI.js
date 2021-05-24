c4be83820e3bbc589e6c3cfa7e4779dd4a9c7073
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-05-23 21:19:13 -0300

    projeto stranger things

---
+++ 3_desenvolvimento_back_end/bloco_29/dia_3/sd-07-stranger-things-frontend/src/services/charactersAPI.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+class CharactersService {
+  constructor({
+    url,
+    timeout,
+  }) {
+    this.http = axios.create({
+      baseURL: url,
+      timeout,
+    });
+  }
+
+  async getCharacters(name, page, size) {
+    const params = {
+      page,
+      size,
+      name,
+    };
+
+    return this.http.get('/', { params });
+  }
+}
+
+export default CharactersService;
